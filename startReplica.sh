13a039e20cb371fcf4b51175712852212406f63b
Runchao Han <hanrc@node-1.hl8.blockchainanom.emulab.net>
2017-10-02 12:55:20 -0600

    1

--- startReplica.sh
+++ startReplica.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-java -cp dist/BFT-Proxy.jar:dist/* bft.BFTNode $@
+
+FABRIC_PATH=$GOPATH/src/github.com/hyperledger/fabric
+START_COMMAND="java -cp dist/BFT-Proxy.jar:lib/* bft.BFTNode"
+
+$START_COMMAND $1 10 $FABRIC_PATH/msp/sampleconfig/signcerts/peer.pem $FABRIC_PATH/msp/sampleconfig/keystore/key.pem 1000,2000
